fdc29d840f7f6ffbd6962302aa8e7ffdea8fd58a
soylent-sys <my-angle-0@siren.ocn.ne.jp>
2023-04-03 01:25:44 +0900

    indexページのレイアウト、newページ作成、予定新規登録（create）機能の実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,11 +1,22 @@
 class SchedulesController < ApplicationController
   def index
+    @schedules = Schedule.all
+    @date = Date.current.strftime('%Y年 %m月 %d日')
   end
 
   def new
+    @schedule = Schedule.new
   end
 
   def create
+    @schedule = Schedule.new(params.require(:schedule).permit(:title, :start_date, :end_date, :all_day, :memo))
+    if @schedule.save
+      flash[:notice] = "新しい予定を登録しました"
+      redirect_to schedules_url
+    else
+      flash.now[:notice] = "予定の登録に失敗しました"
+      render 'new'
+    end
   end
 
   def show
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,13 @@
 class Schedule < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :start_date, presence: true,
+            format: { with: /\A\d{4}\-\d{2}\-\d{2}\Z/, message: "YYYY/MM/DDのフォーマットではありません" }
+  validates :end_date, presence: true,
+            format: { with: /\A\d{4}\-\d{2}\-\d{2}\Z/, message: "YYYY/MM/DDのフォーマットではありません" }
+  validates :memo, length: { maximum: 500 }
+  validate :start_end_check  # 終了日が開始日より前に設定された場合の制御
+
+  def start_end_check
+    errors.add(:end_date, "終了日は開始日の前には設定できません") if self.start_date > self.end_date
+  end
 end
